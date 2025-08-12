fe104f2b4fe9a62217efc2b5a91326bdb38706b6
임재원 <nemo0824@naver.com>
2025-08-12 15:02:52 +0900

    refactor: providers 관심사 분리 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MswProvider from "./msw-provider";
+import { Providers } from "./libs/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MswProvider>{children}</MswProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
