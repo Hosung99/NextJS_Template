605a2747416a502a2a3a82e436788077e5b668c4
손성호 <sonseongho@sonseonghoui-MacBookAir.local>
2025-08-20 14:37:14 +0900

    feat: github 로그인기능 추가

---
+++ src/app/libs/actions/auth.ts
@@ -0,0 +1,11 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+
+export const login = async () => {
+  redirect('/api/auth/signin/github?callbackUrl=/');
+};
+
+export const logout = async () => {
+  redirect('/api/auth/signout?callbackUrl=/');
+};
