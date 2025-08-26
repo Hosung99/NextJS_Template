828fc06710139753c4f314c16c3f8c10487c28ad
임재원 <nemo0824@naver.com>
2025-08-26 18:45:06 +0900

    feature: 기본로그인, token, session 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,69 @@
 import NextAuth from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 
-const handler = NextAuth({
+import { request } from '@/utils/api/request';
+
+import { LoginResponse } from '../../../../services/auth/dto';
+
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'userName', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        try {
+          const loginData = {
+            username: credentials.username,
+            password: credentials.password,
+          };
+          const user = await request<LoginResponse>('POST', '/login', loginData);
+          return user;
+        } catch (error) {
+          console.error('로그인 중 오류 발생:', error);
+          return null;
+        }
+      },
+    }),
   ],
-});
+  callbacks: {
+    async jwt({ token, user }) {
+      try {
+        if (user) {
+          token.id = user.id;
+          token.accessToken = user.accessToken;
+          token.refreshToken = user.refreshToken;
+          token.role = user.role;
+        }
+      } catch (error) {
+        console.error('jwt callback error:', error);
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.accessToken = token.accessToken;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
